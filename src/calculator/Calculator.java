63cf11c4e624fd36ba4b1746e7ba909301c2c9ca
Abod Abu samra <mmmoha848@gmail.com>
2026-09-12 13:11:26 +0300

    Add Java Calculator project

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author abod
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean x=false,y=false,z;
+     //  z = x||y;
+     
+     z= 
+     
+     
+        System.out.println(z);
+       
+       
+       
+    }
+    
+}
